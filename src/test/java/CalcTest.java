fee22819310429d786142ea6ef7ef639b190c71a
Reinaldo Manzano Junior <reinaldo.manzano@gmail.com>
2019-11-29 17:16:21 -0300

    unit tests comentados

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -2,15 +2,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * JUnit tests para a classe Calc que representa uma simples calculadora
+ */
 class CalcTest {
 
     @Test
     void testSomar() {
+        // Instancia a classe Calc
         Calc c = new Calc();
+        // Chama o método somar da classe e armazena na variável res
         int res = c.somar(1,1);
+        // Com o Junit realizamos o unit test para ter certeza que a implementação está conforme esperado
         assertEquals(2, res);
     }
 
+    /**
+     * Testes unitários conforme acima para o método Subtrair
+     */
     @Test
     void testSubtrair() {
         Calc c = new Calc();
@@ -18,6 +27,9 @@ class CalcTest {
         assertEquals(0, res);
     }
 
+    /**
+     * Testes unitários conforme acima para o método Dividir
+     */
     @Test
     void testDividir() {
         Calc c = new Calc();
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -2,12 +2,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Classe de teste unitário para a Classe Person
+ */
 class PersonTest {
 
     @Test
     void testShowNameAndAgeMethod() {
+        // Instancia uma classe Person com alguns parametros desejados
         Person p = new Person("Gilson", 20);
+        // Chama o método showNameAndAge da classe e armazena a mensagem de saída em uma variável
         final String nameAndAge = p.showNameAndAge();
+
+        // Com Junit fazemos o assert para ter certeza que não é Null o resultado e que
+        // a mensagem tem a saída e conteúdo esperado
         assertNotNull(nameAndAge);
         assertEquals("Gilson-20", nameAndAge);
     }
